f237ed858d80a6095a313904801b395377c9ee8c
wailaixing <2912354128@qq.com>
2016-05-27 20:54:09 +0800

    5/27 20:54 设置安全号码

--- src/com/wailaixing/mobilesafe/setup3Activity.java
+++ src/com/wailaixing/mobilesafe/setup3Activity.java
@@ -2,25 +2,46 @@ package com.wailaixing.mobilesafe;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class setup3Activity extends BaseSetupActivity {
+	
+	
+	private EditText et_setup3_phone;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_setup3);
+		et_setup3_phone=(EditText)findViewById(R.id.et_setup3_phone);
+		et_setup3_phone.setText(sp.getString("safenumber", ""));
+		
 	}
 	
 
 	@Override
 	public void showNext() {
+		//必须保存安全号码
+		String phone=et_setup3_phone.getText().toString().trim();
+		if(TextUtils.isEmpty(phone)){
+			Toast.makeText(this, "安全号码未设置", 1).show();
+			return ;
+		}
+		
+		Editor editor=sp.edit();
+		editor.putString("safenumber", phone);
+		editor.commit();
+		
+		
 		Intent intent = new Intent(this,setup4Activity.class);
 		startActivity(intent);
 		finish();
 		overridePendingTransition(R.anim.tran_in, R.anim.tran_out);
-		
 		
 	}
 
@@ -37,9 +58,22 @@ public class setup3Activity extends BaseSetupActivity {
 	public void selectContact(View view){
 		Intent intent=new Intent(this,SelectContactActivity.class);
 		startActivityForResult(intent, 0);
-		finish();
+
 		
 	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// TODO Auto-generated method stub
+		super.onActivityResult(requestCode, resultCode, data);
+		if(data==null){
+			return;
+		}
+		String phone=data.getStringExtra("phone").replace("-", "");
+		
+		et_setup3_phone.setText(phone);
+	}
+	
 	
 
 }
